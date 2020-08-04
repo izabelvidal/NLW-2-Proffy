3a5ab685d114cd78c6dcc0b0cc8008b8a76780c0
Izabel Vidal <izabelvidal99@gmail.com>
2020-08-04 14:02:03 -0300

    parei para a mentoria da microsoft

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -4,22 +4,29 @@ import { Link } from 'react-router-dom';
 import backIcon from '../../assets/images/icons/back.svg';
 import logoImg from '../../assets/images/logo.svg';
 
-function PageHeader(){
+import './styles.css'
+
+interface PageHeadeProps {
+    title: string;
+}
+
+const PageHeader: React.FC<PageHeadeProps> = (props) => {
     return(
         <header className="page-header">
-                <div className="top-bar-container">
-                    <Link to="/">
-                        <img src={backIcon} alt="Voltar"/>
-                    </Link>
-                    <img src={logoImg} alt="Proffy"/>
-                </div>
-                <div className="header-content">
-                    <strong>
-                        Estes são os proffys disponíveis
-                    </strong>
-                </div>
-            </header>
-    )
+            <div className="top-bar-container">
+                <Link to="/">
+                    <img src={backIcon} alt="Voltar"/>
+                </Link>
+                <img src={logoImg} alt="Proffy"/>
+            </div>
+            <div className="header-content">
+                <strong>
+                    {props.title}
+                </strong>
+                {props.children}
+            </div>
+        </header>
+    );
 }
 
 export default PageHeader;
